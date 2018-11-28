2ab83b9a77616dede3def94445244d9a904a3712
ArtyemTS <tsibenkov.artyem@gmail.com>
2018-11-28 22:14:58 +0300

    Делали вместе

--- src/main/java/learn/epam/mlhh/controllers/HelloController.java
+++ src/main/java/learn/epam/mlhh/controllers/HelloController.java
@@ -7,17 +7,24 @@ import java.util.Map;
 @Controller
 public class HelloController {
     private String message = "Form";
+    //   private String message1 = "Search";
 
     @RequestMapping(value="/", method=RequestMethod.GET)
     public String home(Map<String, Object> model) {
         model.put("message", this.message);
         return "index";
     }
-
     @RequestMapping(value="/", method=RequestMethod.POST)
     public String homePost(Map<String, Object> model) {
         model.put("message", "Method Post");
+       // return "search_page";
         return "post";
+    }
+
+    @RequestMapping(value="/search", method=RequestMethod.POST)
+    public String searchPost(Map<String, Object> model) {
+        model.put("message", "Method Post");
+        return "search_page";
     }
 
 }
